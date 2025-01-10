86a37b1448f92855a4563259d0de68fec47a60d7
Judit Horvath <judit.horvath@appliedtechnology.se>
2025-01-11 00:32:17 +0100

    feat: add Get wish function

--- Backend/TreWishApi/Controllers/WishesController.cs
+++ Backend/TreWishApi/Controllers/WishesController.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Backend.Data;
 using Microsoft.AspNetCore.Mvc;
+using TreWishApi.Models;
+using TreWishApi.Models.Dtos;
 
 namespace TreWishApi.Controllers
 {
@@ -9,6 +12,33 @@ namespace TreWishApi.Controllers
     [Route("api/[controller]")]
     public class WishesController : ControllerBase
     {
-        
+        private readonly ApplicationDbContext _context;
+        public WishesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private WishResponse WishToResponse(Wish wish)
+        {
+
+            return new WishResponse()
+            {
+                Name = wish.Name,
+                Description = wish.Description,
+                Price = wish.Price,
+                WebPageLink = wish.WebPageLink
+            };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Wish> Get(int id)
+        {
+            var wish = _context.Wishes.Find(id);
+            if (wish is null)
+            {
+                return NotFound();
+            }
+            return Ok(WishToResponse(wish));
+        }
     }
 }
